b8ddec62f2c2b4edd94be36bb07774d7bd814e2a
Benjamin Odion <BenjaminOwase@gmail.com>
2026-08-04 12:40:30 +0100

    feat: add secure deployment admin setup

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ pip install -r requirements.txt
 
 # Run migrations and collect static files
 python manage.py migrate
+python manage.py ensure_deployment_admin
 python manage.py collectstatic --noinput
-
